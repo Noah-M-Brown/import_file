9500453cb9e75334ea9aa88bcf2e985241f9b014
wowsk <wowsk14159@gmail.com>
2015-09-28 06:45:59 -0500

    Update get.js

--- get.js
+++ get.js
@@ -2,6 +2,10 @@ function getStyle(program_id) {
   var styleEl=document.getElementsByTagName("style")[0]
   console.log(styleEl)
   $.getJSON("https://www.khanacademy.org/api/internal/show_scratchpad?scratchpad_id="+program_id+"&casing=camel&topic_slug=computer-programming&lang=en&_=1443392559115&callback=?",function(data) {
+    if(styleEl===undefined) {
+      var styleEl=document.createElement("style");
+      document.head.appendChild(styleEl);
+    }
     styleEl.innerHTML=data.scratchpad.revision.code; 
   });
 }
