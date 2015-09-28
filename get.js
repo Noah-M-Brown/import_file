418ed521723d72d1fe914866694b5d86b4c19723
wowsk <wowsk14159@gmail.com>
2015-09-28 06:46:46 -0500

    Update get.js

--- get.js
+++ get.js
@@ -1,9 +1,8 @@
 function getStyle(program_id) {
-  var styleEl=document.getElementsByTagName("style")[0]
-  console.log(styleEl)
+  var styleEl=document.getElementsByTagName("style")[0];
   $.getJSON("https://www.khanacademy.org/api/internal/show_scratchpad?scratchpad_id="+program_id+"&casing=camel&topic_slug=computer-programming&lang=en&_=1443392559115&callback=?",function(data) {
     if(styleEl===undefined) {
-      var styleEl=document.createElement("style");
+      styleEl=document.createElement("style");
       document.head.appendChild(styleEl);
     }
     styleEl.innerHTML=data.scratchpad.revision.code; 
